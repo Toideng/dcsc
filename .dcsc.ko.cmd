3f9c6a2c92e52ba959d48e9e6b9000c676e004e5
Toideng <toideng@toideng.com>
2020-05-25 07:13:44 +0000

    commit

--- .dcsc.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/dcsc/dcsc.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /root/dcsc/dcsc.ko /root/dcsc/dcsc.o /root/dcsc/dcsc.mod.o
--- .tmp_versions/dcsc.mod
+++
@@ -1,2 +0,0 @@
-/root/dcsc/dcsc.ko
-/root/dcsc/dcsc.o
